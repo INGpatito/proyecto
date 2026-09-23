59fd6b64b78dab08261cc3e26aef601d1e0e61a3
INGpatito <INGpatito@users.noreply.github.com>
2026-09-23 06:44:39 -0600

    ya perdi la cuenta pero pruba global de la coneccion con la base de datos

--- coneccion usuarios/conexion.php
+++ coneccion usuarios/conexion.php
@@ -1,7 +1,14 @@
 <?php
 // Conexion simple a MariaDB / MySQL
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+
+// Responder con exito si el navegador hace una peticion preflight OPTIONS
+if ($_SERVER["REQUEST_METHOD"] === "OPTIONS") {
+    http_response_code(200);
+    exit();
+}
 
 $conexion = mysqli_connect("localhost", "root", "pato", "comida");
 
